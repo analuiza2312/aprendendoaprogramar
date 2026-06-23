ace800d5936c0c3eacefdfb5b098941c1591ab23
ana luiza <analuiza.rochacoelho09@gmail.com>
2026-06-23 11:40:55 -0300

    main

---
+++ aula 7/exemplo.js
@@ -0,0 +1,16 @@
+let aluno= {
+nome : "ana",
+idade : 20,
+endereço: {
+logradouro: "Rua do Piano",
+numeros: 44,
+complemento: "casa",
+cep: 25225218,
+bairro: "Campos eliseos",
+cidade: "duque de caxias",
+uf: "RJ"}
+
+}
+
+console.log(aluno.nome)
+console.log(aluno.endereço.logradouro)
